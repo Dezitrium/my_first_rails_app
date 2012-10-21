b4bd8b4dde9db1d88a7ba249960d52b4e946667a
Frederick Straller <freddy.s88@gmx.net>
2012-10-21 17:57:17 +0200

    Add partial profile site and signup mechanism

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,24 @@
 module UsersHelper
+  
+  def avatar_for(user)
+    user.name
+  end
+
+  # Gravatar (http://gravatar.com/) 
+  DEFAULT_GRAVATAR_OPTIONS = {
+      size:'32'
+  }
+
+  def gravatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+     "https://secure.gravatar.com/avatar/#{gravatar_id}"    
+  end
+
+  def gravatar_for user, options={}
+    options = DEFAULT_GRAVATAR_OPTIONS.merge options
+    options[:alt]  = avatar_for user
+    options[:size] = "%{size}x%{size}" % options  # image_tag expects e.g. "48x48"    
+    image_tag gravatar_url(user), options
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 MyFirstApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
