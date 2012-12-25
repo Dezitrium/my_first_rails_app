0a1eac9403411c1e290780868180566578079e27
Frederick Straller <frederick.straller@uni-ulm.de>
2012-12-25 02:49:11 +0100

    Fix for Windows use

--- db/migrate/20121020112004_create_users.rb
+++ db/migrate/20121020112004_create_users.rb
@@ -7,13 +7,13 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
     
-    add_index :email, unique: true
+    add_index :users, :email, unique: true
 
-    add_column :password_digest, :string
+    add_column :users, :password_digest, :string
 
-    add_column :remember_token, :string
-    add_index  :remember_token
+    add_column :users, :remember_token, :string
+    add_index  :users, :remember_token
 
-    add_column :admin, :boolean, default: false
+    add_column :users, :admin, :boolean, default: false
   end
 end
